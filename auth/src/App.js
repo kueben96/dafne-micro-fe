a75e899e9ba3c694c9f4c1c41b4ff86172788841
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-03-13 15:52:44 +0100

    play with components

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button, createTheme, ThemeProvider, Typography } from '@mui/material'
+import DataArrayIcon from '@mui/icons-material/DataArray';
+import { Box, Container } from '@mui/system'
+import styled from '@emotion/styled'
 
+import { theme } from './theme'
 const App = () => {
+    const PinkButton = styled(Button)(({ theme }) => ({
+        backgroundColor: theme.palette.neutral.black,
+        color: theme.palette.neutral.white,
+    }));
     return (
-        <h1>Auth App</h1>
+        <ThemeProvider theme={theme}>
+            <Container >
+                <h1>Auth App</h1>
+                <Button variant='outlined' >Button outlined</Button>
+                <Box>Box</Box>
+                <Button color="neutral" startIcon={<DataArrayIcon />} variant='contained'>Button contained</Button>
+                <Typography variant='h2'>h1 </Typography>
+                <Button variant='contained' sx={{
+                    backgroundColor: "pink",
+                    margin: 5,
+                    "&:hover": {
+                        backgroundColor: "red"
+                    }
+                }}>custom button</Button>
+                <PinkButton variant='contained'>CompPink</PinkButton>
+            </Container>
+        </ThemeProvider>
     )
 }
 
---
+++ auth/src/theme.js
@@ -0,0 +1,40 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+    components: {
+        MuiTypography: {
+            defaultProps: {
+                variantMapping: {
+                    h1: 'h2',
+                    h2: 'h2',
+                    subtitle1: 'h2',
+                    subtitle2: 'h3'
+                }
+            }
+        }
+    },
+    palette: {
+        type: 'light',
+        primary: {
+            dark: '#3C908',
+            main: '#6CC1B5',
+            light: '#A9DBD4',
+            lighter: '#E2F3F1'
+        },
+        secondary: {
+            dark: '#C2631E',
+            main: '#E08542',
+            light: '#E9A372',
+            lighter: '#F1CBB0'
+        },
+        neutral: {
+            black: '#1D201F',
+            white: '#FFFFFF',
+        },
+        grey: {
+            regular: '#656565',
+            light: '#B9B9B9',
+            lighter: '#EDEDED'
+        }
+    },
+})
