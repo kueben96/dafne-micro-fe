bd94b713849109f878ae4031df1bba39d1e8d595
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-04-11 16:30:48 +0300

    remove jsx routing from marketing app.js

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -4,11 +4,6 @@ import { Link, Routes, Route } from 'react-router-dom'
 const App = () => {
     return (
         <div>
-            <Routes>
-                <Route path="/contribute" />
-                <Route path="/about" />
-                <Route path="/documentation" />
-            </Routes>
             <h1>Marketing App</h1>
             {/* Communicate back to Container/parent */}
             <Link to='/auth'>Auth</Link>
