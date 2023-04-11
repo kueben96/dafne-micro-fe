564ae938c11f25ccae24871eaa7674b34b030cec
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-04-11 12:44:23 +0300

    change mount function of marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './routing/routes'
+import { createRouter } from './routing/router-factory'
 
 // mount functin to start up the app
 
-const router = createBrowserRouter(routes);
+// const router = createBrowserRouter(routes);
 
-const mount = (el) => {
+const mount = ({
+    mountPoint,
+    initialPathname,
+    routingStrategy
+} = {}) => {
+    const router = createRouter({
+        strategy: routingStrategy || 'browser',
+        initialPathname: initialPathname || '/',
+    })
     ReactDOM.render(
         <RouterProvider router={router} />,
-        el
+        mountPoint
     )
+    // return () => queueMicrotask(() => ReactDOM.unmountComponentAtNode(mountPoint));
 }
 // if in dev or isolation -> call mount immediately 
 
@@ -20,7 +29,7 @@ if (process.env.NODE_ENV == 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount({ mountPoint: devRoot, routingStrategy: 'browser' });
     }
 }
 
