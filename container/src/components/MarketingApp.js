ab0ca8b035ae4e9f021ac6715cab76b121b0012f
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-04-11 14:42:18 +0300

    add marketingApp mounting

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,14 +1,65 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "../../../marketing/src/bootstrap";
+import { useNavigate, useLocation } from 'react-router-dom'
+
+const marketingBaseName = 'marketing'
 
 const MarketingApp = () => {
-    const ref = useRef(null);
+    const wrapperRef = useRef(null);
+    const navigate = useNavigate();
+    const location = useLocation();
 
+
+    // listens for navigation events and updates the location object accordingly
     useEffect(() => {
-        mount(ref.current)
-    });
+        const marketingAppNavigationHandler = (event) => {
+            const pathname = event.detail;
+            const newPathname = `${marketingBaseName}${pathname}`
+            if (newPathname === location.pathname) {
+                return;
+            }
+            navigate(newPathname);
+
+        }
+        window.addEventListener("[marketing] navigated", marketingAppNavigationHandler);
+
+        return () => {
+            window.removeEventListener(
+                "[marketing] navigated",
+                marketingAppNavigationHandler
+            )
+        }
+    }, [location]);
+
+    // listens for location changes in the shell and dispatches notification to marketing app if the location starts with marketingBaseName
+    useEffect(() => {
+        if (location.pathname.startsWith(marketingBaseName)) {
+            console.log("useeffeeec")
+            window.dispatchEvent(
+                new CustomEvent("[shell] navigated", {
+                    detail: location.pathname.replace(marketingBaseName, ""),
+                })
+            );
+        }
+    }, [location]);
+
+    const isFirstRunRef = useRef(true);
+    const unmountRef = useRef(() => { });
+    useEffect(() => {
+        if (!isFirstRunRef.current) {
+            return;
+        }
+        unmountRef.current = mount({
+            mountPoint: wrapperRef.current,
+            initialPathname: location.pathname.replace(marketingBaseName, ""),
+        });
+        isFirstRunRef.current = false;
+    }, [location]);
+
+    useEffect(() => unmountRef.current, []);
     return (
-        <div ref={ref}></div>
+
+        <div ref={wrapperRef} id="marketing-mfe" />
     )
 }
 
