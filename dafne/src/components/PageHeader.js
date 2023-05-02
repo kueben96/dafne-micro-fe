4f08d2e5720f681e77798329b6000b1cc19e007a
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-05-02 11:57:58 +0200

    make process name editable

--- dafne/src/components/PageHeader.js
+++ dafne/src/components/PageHeader.js
@@ -1,12 +1,45 @@
-import { AppBar, Box, Container, Divider, Tab, Tabs, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Box, Container, Divider, Icon, IconButton, Tab, Tabs, TextField, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import HeaderBreadcrumbs from './HeaderBreadcrumbs';
 import { pageHeaderStyles } from '../styles/dafneStyles';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
 
 
 
 const PageHeader = (props) => {
+
     const classes = pageHeaderStyles()
+    const [isEditable, setIsEditable] = useState(false);
+    const [title, setTitle] = useState(props.title);
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleTitleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            setIsEditable(false);
+            // TODO: handle state/session object of process instance
+            // props.onTitleChange(title);
+        }
+    };
+
+    const handleEditClick = () => {
+        setIsEditable(true);
+    };
+
+    const handleCancelClick = () => {
+        setIsEditable(false);
+        setTitle(props.title);
+    };
+
+    const handleSaveClick = () => {
+        setIsEditable(false);
+        // TODO: handle state/session object of process instance
+        //   props.onTitleChange(title);
+    };
+
 
     return (
         <AppBar position="static" className={classes.appBar}>
@@ -16,10 +49,32 @@ const PageHeader = (props) => {
                         <Box>
                             <HeaderBreadcrumbs />
                         </Box>
-                        <Box>
-                            <Typography variant="h4" fontWeight="bold" component="div" sx={{ paddingTop: 1, paddingBottom: 1 }}>
-                                {props.title}
-                            </Typography>
+                        <Box display="flex" flexDirection="row" alignItems="center">
+                            {isEditable ? (
+                                <>
+                                    <TextField
+                                        autoFocus
+                                        margin="none"
+                                        value={title}
+                                        onChange={handleTitleChange}
+                                        onKeyPress={handleTitleKeyPress}
+                                        onBlur={handleCancelClick}
+                                        sx={{ width: '70%' }}
+                                    />
+                                    <IconButton onClick={handleSaveClick}>
+                                        <CheckOutlinedIcon sx={{ padding: '1px' }} />
+                                    </IconButton>
+                                </>
+                            ) : (
+                                <>
+                                    <Typography variant="h4" fontWeight="bold" component="div" sx={{ paddingTop: 1, paddingBottom: 1 }}>
+                                        {title}
+                                    </Typography>
+                                    <IconButton onClick={handleEditClick}>
+                                        <BorderColorOutlinedIcon sx={{ padding: '1px' }} />
+                                    </IconButton>
+                                </>
+                            )}
                         </Box>
                     </Box>
                 </Container>
