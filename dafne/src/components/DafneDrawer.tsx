4781602ab99e3ae8107d616f8604220c9edfc3c8
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-05-19 16:09:05 +0200

    conver drawer to tsx

---
+++ dafne/src/components/DafneDrawer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import NavComponents from './NavComponents';
+import { Drawer, useTheme } from '@mui/material';
+import { drawerStyles } from '../assets/theme/dafneStyles';
+
+interface DafneDrawerProps {
+  handleDrawerToggle: () => void;
+  isNavOpen: boolean;
+}
+
+const DafneDrawer: React.FC<DafneDrawerProps> = ({ handleDrawerToggle, isNavOpen }) => {
+  const classes = drawerStyles();
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={isNavOpen}
+      onClose={handleDrawerToggle}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <NavComponents />
+    </Drawer>
+  );
+};
+
+export default DafneDrawer;
