1a40ab2e3e551bd5bd18d25e2fd94faa9cf0714c
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-05-19 15:48:23 +0200

    convert horizontal stepper to ts

---
+++ dafne/src/components/HorizontalStepper.tsx
@@ -0,0 +1,55 @@
+import { Stepper, Step, StepLabel, Typography, Theme } from '@mui/material';
+import React from 'react';
+import CustomStepIcon from './CustomStepIcon';
+
+interface HorizontalStepperProps {
+  activeStep: number;
+  isStepCompleted: (step: number) => boolean;
+  steps: string[];
+  theme: Theme;
+  width: string;
+}
+
+const HorizontalStepper: React.FC<HorizontalStepperProps> = ({
+  activeStep,
+  isStepCompleted,
+  steps,
+  theme,
+  width,
+}) => {
+  return (
+    <Stepper sx={{ width: width }} activeStep={activeStep}>
+      {steps.map((label, index) => {
+        const stepProps: any = {};
+        const labelProps: any = {};
+
+        if (isStepCompleted(index)) {
+          stepProps.completed = true;
+        }
+
+        return (
+          <Step key={label} {...stepProps}>
+            <StepLabel
+              {...labelProps}
+              StepIconComponent={(props) => (
+                <CustomStepIcon
+                  {...stepProps}
+                  icon={index + 1}
+                  completed={stepProps.completed}
+                  active={activeStep === index}
+                  theme={theme}
+                >
+                  {index + 1}
+                </CustomStepIcon>
+              )}
+            >
+              {label}
+            </StepLabel>
+          </Step>
+        );
+      })}
+    </Stepper>
+  );
+};
+
+export default HorizontalStepper;
