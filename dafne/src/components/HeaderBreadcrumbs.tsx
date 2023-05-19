3fe5069d4ead472d89ac7f95bd8ae04ed01f819e
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-05-19 16:00:11 +0200

    convert header breadcrumbs

---
+++ dafne/src/components/HeaderBreadcrumbs.tsx
@@ -0,0 +1,21 @@
+import { Breadcrumbs, Link, Typography, styled, Theme } from '@mui/material';
+import React from 'react';
+
+const CustomBreadcrumbs = styled(Breadcrumbs)(({ theme }: { theme: Theme }) => ({
+  color: theme.palette?.gray?.main,
+  fontSize: theme.typography.subtitle1.fontSize,
+}));
+
+// TODO: add links with props
+const HeaderBreadcrumbs: React.FC = (props) => {
+  return (
+    <CustomBreadcrumbs aria-label="breadcrumb">
+      <Link underline="hover" color="inherit" href="/">
+        Dashboard
+      </Link>
+      <Typography variant="subtitle1">Processes</Typography>
+    </CustomBreadcrumbs>
+  );
+};
+
+export default HeaderBreadcrumbs;
