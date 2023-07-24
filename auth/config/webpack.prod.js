a734bb12831b1a9bac913538eabf495cf78beb7f
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-07-24 17:35:37 +0200

    created container deploy script

---
+++ auth/config/webpack.prod.js
@@ -0,0 +1,30 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            name: 'dafne',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './AuthApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies,
+        })
+    ]
+}
+
+
+module.exports = merge(commonConfig, prodConfig);
