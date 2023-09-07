5f9cd988937a7e0816e170d210b523dc587fb22f
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-09-07 15:34:14 +0200

    prevent resize observer loop error which appears in dev mode

--- dafne/src/components/ProcessesTable.tsx
+++ dafne/src/components/ProcessesTable.tsx
@@ -25,9 +25,40 @@ interface ProcessesTableProps {
 
 
 const ProcessesTable: React.FC<ProcessesTableProps> = ({ rows, columns }) => {
+    const tableRef = React.useRef<HTMLDivElement | null>(null);
+
+    React.useEffect(() => {
+        const handleResize = () => {
+            // Your resize handling logic goes here.
+            // This function will be called when the table container's size changes.
+        };
+
+        // Create a ResizeObserver to watch for changes in the table container's size.
+        const resizeObserver = new ResizeObserver((entries) => {
+            // Call handleResize whenever the size changes.
+            handleResize();
+
+            // If needed, you can access the new size like this:
+            const [entry] = entries;
+            const { width, height } = entry.contentRect;
+            console.log(`New size: width=${width}, height=${height}`);
+        });
+
+        // Attach the ResizeObserver to the table container element.
+        if (tableRef.current) {
+            resizeObserver.observe(tableRef.current);
+        }
+
+        // Clean up the observer when the component unmounts.
+        return () => {
+            if (tableRef.current) {
+                resizeObserver.unobserve(tableRef.current);
+            }
+        };
+    }, []);
 
     return (
-        <TableCustomBox>
+        <TableCustomBox ref={tableRef}>
             <CustomDataGrid
                 autoHeight
                 rows={rows ?? []}
@@ -39,6 +70,8 @@ const ProcessesTable: React.FC<ProcessesTableProps> = ({ rows, columns }) => {
             />
         </TableCustomBox>
     );
+
+
 };
 
 export default ProcessesTable;
