e07cad19dc3c06cb4311e1c716ae51c31ec25240
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-09-12 16:55:59 +0200

    catch error in snackbar

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,19 +1,20 @@
-import { Box, TextField, Typography, Button, useTheme } from '@mui/material';
+import { Box, TextField, Typography, Button, useTheme, Snackbar, Alert } from '@mui/material';
 import React, { useState } from 'react'
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { formContainerStyles } from '../styles/authStyles';
-import { useEffect } from 'react';
 const LoginForm = () => {
 
-    const fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-
     const theme = useTheme()
-
     const [formData, setFormData] = useState({
         email: 'user',
         password: 'user_pass',
     });
+    const [error, setError] = useState(null);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
     const handleInputChange = (event) => {
         setFormData((prevFormData) => ({
             ...prevFormData,
@@ -24,40 +25,47 @@ const LoginForm = () => {
 
 
     const handleSubmit = async (event) => {
-
         event.preventDefault();
 
-        const token = await handleLogin({ email: formData.email, password: formData.password })
-        console.log(token)
-        // TODO: validate form data
-        // if (validateFormData()) {
-        //     // Submit the form data to Keycloak for user registration
-        // }
-        // Store the JWT token in the localStorage
-        localStorage.setItem('jwtToken', token);
+        try {
+            const token = await handleLogin({
+                email: formData.email,
+                password: formData.password,
+            });
 
-        // Dispatch a custom event to notify other microfrontends
-        window.dispatchEvent(new CustomEvent('jwtReceived', { detail: fakeToken }));
+            // Store the JWT token in the localStorage
+            localStorage.setItem('jwtToken', token);
 
+            // Dispatch a custom event to notify other microfrontends
+            window.dispatchEvent(new CustomEvent('jwtReceived', { detail: token }));
+        } catch (error) {
+            setError(error.message);
+            setSnackbarOpen(true);
+        }
     };
 
-    // // create async function for handle login 
+
     const handleLogin = async ({ email, password }) => {
         const response = await fetch('http://localhost:8086/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json',
+                Accept: 'application/json',
             },
             body: JSON.stringify({
                 user: email,
                 user_pass: password,
             }),
-        })
+        });
 
-        const data = await response.json()
-        return data.access_token
-    }
+        if (!response.ok) {
+            throw new Error('Login failed. Please check your credentials.');
+        }
+
+        const data = await response.json();
+        return data.access_token;
+    };
+
     return (
 
         <Box sx={formContainerStyles}>
@@ -96,6 +104,19 @@ const LoginForm = () => {
                     </Button>
                 </Box>
             </form>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={snackbarOpen}
+                autoHideDuration={5000}
+                color='secondary'
+            >
+                <Alert color='secondary' onClose={handleSnackbarClose} severity="error" sx={{ width: '100%' }}>
+                    {error}
+                </Alert>
+            </Snackbar>
         </Box>
     )
 }
