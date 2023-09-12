296ffaf45f4cbf10692b5cedb3d419577171dcb7
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-09-12 15:52:57 +0200

    get response from wiremock

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,27 @@ import { Box, TextField, Typography, Button, useTheme } from '@mui/material';
 import React, { useState } from 'react'
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { formContainerStyles } from '../styles/authStyles';
+import { useEffect } from 'react';
 const LoginForm = () => {
+
+    useEffect(() => {
+        // fetch('https://jsonplaceholder.typicode.com/todos/1')
+        //     .then(response => response.json())
+        //     .then(json => console.log(json))
+        fetch('http://localhost:8086/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                "Access-Control-Allow-Origin": "http://localhost:8086"
+            },
+            body: JSON.stringify({
+                user: "user",
+                user_pass: "user_pass",
+            }),
+        })
+            .then(response => console.log(response))
+    }, []); // The empty dependency array means this effect runs once, like componentDidMount
 
     const fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
@@ -23,7 +43,9 @@ const LoginForm = () => {
 
 
     const handleSubmit = (event) => {
+
         event.preventDefault();
+        // handleLogin().then(response => console.log(response.json()))
         // TODO: validate form data
         // if (validateFormData()) {
         //     // Submit the form data to Keycloak for user registration
@@ -33,7 +55,45 @@ const LoginForm = () => {
 
         // Dispatch a custom event to notify other microfrontends
         window.dispatchEvent(new CustomEvent('jwtReceived', { detail: fakeToken }));
+
     };
+
+    // // create async function for handle login 
+    // const handleLogin = async () => {
+    //     const response = await fetch('http://localhost:8086/api/login', {
+    //         method: 'POST',
+    //         mode: 'no-cors', // no-cors, *cors, same-origin
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json',
+    //             "Access-Control-Allow-Origin": "*"
+    //         },
+    //         body: JSON.stringify({
+    //             user: "user",
+    //             user_pass: "user_pass",
+    //         }),
+    //     })
+    //     return response
+    // }
+
+
+
+
+    // useEffect(() => {
+    //     fetch('http://localhost:8086/api/login', {
+    //         method: 'POST',
+    //         mode: 'no-cors', // no-cors, *cors, same-origin
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json',
+    //             "Access-Control-Allow-Origin": "*"
+    //         },
+    //         body: JSON.stringify({
+    //             user: "user",
+    //             user_pass: "user_pass",
+    //         }),
+    //     }).then(response => console.log(response.json()))
+    // }, [])
 
     return (
 
