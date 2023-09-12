fbdb63367d66bb8488838c62b513cbeb2b406421
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-09-12 16:39:59 +0200

    handle login with token

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -5,32 +5,13 @@ import { formContainerStyles } from '../styles/authStyles';
 import { useEffect } from 'react';
 const LoginForm = () => {
 
-    useEffect(() => {
-        // fetch('https://jsonplaceholder.typicode.com/todos/1')
-        //     .then(response => response.json())
-        //     .then(json => console.log(json))
-        fetch('http://localhost:8086/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                "Access-Control-Allow-Origin": "http://localhost:8086"
-            },
-            body: JSON.stringify({
-                user: "user",
-                user_pass: "user_pass",
-            }),
-        })
-            .then(response => console.log(response))
-    }, []); // The empty dependency array means this effect runs once, like componentDidMount
-
     const fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
     const theme = useTheme()
 
     const [formData, setFormData] = useState({
-        email: '',
-        password: '',
+        email: 'user',
+        password: 'user_pass',
     });
 
     const handleInputChange = (event) => {
@@ -42,16 +23,18 @@ const LoginForm = () => {
     };
 
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
 
         event.preventDefault();
-        // handleLogin().then(response => console.log(response.json()))
+
+        const token = await handleLogin({ email: formData.email, password: formData.password })
+        console.log(token)
         // TODO: validate form data
         // if (validateFormData()) {
         //     // Submit the form data to Keycloak for user registration
         // }
         // Store the JWT token in the localStorage
-        localStorage.setItem('jwtToken', fakeToken);
+        localStorage.setItem('jwtToken', token);
 
         // Dispatch a custom event to notify other microfrontends
         window.dispatchEvent(new CustomEvent('jwtReceived', { detail: fakeToken }));
@@ -59,42 +42,22 @@ const LoginForm = () => {
     };
 
     // // create async function for handle login 
-    // const handleLogin = async () => {
-    //     const response = await fetch('http://localhost:8086/api/login', {
-    //         method: 'POST',
-    //         mode: 'no-cors', // no-cors, *cors, same-origin
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Accept': 'application/json',
-    //             "Access-Control-Allow-Origin": "*"
-    //         },
-    //         body: JSON.stringify({
-    //             user: "user",
-    //             user_pass: "user_pass",
-    //         }),
-    //     })
-    //     return response
-    // }
+    const handleLogin = async ({ email, password }) => {
+        const response = await fetch('http://localhost:8086/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                user: email,
+                user_pass: password,
+            }),
+        })
 
-
-
-
-    // useEffect(() => {
-    //     fetch('http://localhost:8086/api/login', {
-    //         method: 'POST',
-    //         mode: 'no-cors', // no-cors, *cors, same-origin
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Accept': 'application/json',
-    //             "Access-Control-Allow-Origin": "*"
-    //         },
-    //         body: JSON.stringify({
-    //             user: "user",
-    //             user_pass: "user_pass",
-    //         }),
-    //     }).then(response => console.log(response.json()))
-    // }, [])
-
+        const data = await response.json()
+        return data.access_token
+    }
     return (
 
         <Box sx={formContainerStyles}>
