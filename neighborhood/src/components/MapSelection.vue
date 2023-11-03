8ec8c097d532b5cf2955448421654aefdd13b172
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-03 16:33:30 +0100

    clean up neighborhood generation

--- neighborhood/src/components/MapSelection.vue
+++ neighborhood/src/components/MapSelection.vue
@@ -17,19 +17,15 @@
                 <RadioButton v-model="selectionMode" value="city" name="selectionMode" />
                 <label for="city" class="m-2">City selection</label>
                 <div class="maplibregl-ctrl stadiamaps-search-box">
-                    <InputText v-model="selectedCity" class="m-2 input-container" placeholder="Selected City"
-                        @input="syncInputFields" />
-                    <div v-if="searchResultsList" class="results">
-                        <div class="results-list" v-html="searchResultsList.innerHTML"></div>
-                    </div>
+                    <InputText v-model="selectedCity" class="m-2 input-container" placeholder="Selected City" disabled />
                 </div>
 
                 <RadioButton v-model="selectionMode" value="polygon" name="selectionMode" />
                 <label for="city" class="m-2">Polygon selection</label>
-                <Button @click="toggleGeoJSONVisibility" outlined>
+                <Button size="small" style="color: var(--primary-dark);" @click="toggleGeoJSONVisibility" outlined>
                     <span>
                         <i class="pi pi-external-link mr-1"></i>
-                        'Show GeoJSON'
+                        Show GeoJSON
                     </span>
                 </Button>
 
@@ -64,39 +60,20 @@ const selectedAreaGeoJSONText = ref(null);
 const selectionMode = ref("city"); // Initialize with "Select City" mode
 const selectedCity = ref(null);
 const showGeoJSON = ref(false);
-const searchResultsList = ref(null);
 
 let draw = null; // Store the draw instance
 let drawnPolygonId = null;
-let searchControl;
 
 
 function toggleGeoJSONVisibility() {
     showGeoJSON.value = !showGeoJSON.value;
 }
-
-function syncInputFields() {
-
-    const searchControlInput = searchControl.input;
-    if (searchControlInput) {
-        searchControlInput.value = selectedCity.value;
-        const event = new Event("input", { bubbles: true });
-        searchControlInput.dispatchEvent(event);
-        searchResultsList.value = searchControl.resultsList;
-        console.log(searchResultsList.value)
-    }
-    selectedCity.value = searchControlInput.value;
-}
-
-
 onMounted(() => {
 
-    searchControl = new MapLibreSearchControl(
+    const searchControl = new MapLibreSearchControl(
         {
             onResultSelected: (result) => {
                 selectedCity.value = result.properties.label;
-                syncInputFields();
-                console.log(result)
             },
         }
     );
@@ -115,37 +92,34 @@ onMounted(() => {
             trash: true
         }
     });
+
     map.addControl(searchControl, 'top-left');
-
-
-
     map.addControl(draw, 'top-right');
     map.addControl(new maplibre.NavigationControl());
 
+    // Add maplibre classes to style draw controls
     const mapboxCtrlGroup = document.querySelector('.mapboxgl-ctrl-group');
     mapboxCtrlGroup.classList.add('maplibregl-ctrl', 'maplibregl-ctrl-group');
 
     function updateArea(e) {
         const data = draw.getAll();
         selectedAreaGeoJSON.value = data;
-        selectedAreaGeoJSONText.value = JSON.stringify(data, null, 2); // Format GeoJSON for display
+        selectedAreaGeoJSONText.value = JSON.stringify(data, null, 2);
     }
-
+    // DRAW EVENTS
     map.on("draw.delete", updateArea);
     map.on("draw.update", updateArea);
     map.on("draw.create", (e) => {
-        // Handle the newly created polygon
         if (drawnPolygonId) {
             draw.delete(drawnPolygonId)
         }
-        console.log("e.features")
-        console.log(e.features)
         drawnPolygonId = e.features[0].id;
         updateArea()
     });
 }
-
 )
+
+// TODO: implement API Call
 function submitSelection() {
     console.log("submitSelection")
     if (selectionMode.value === "city") {
@@ -170,7 +144,6 @@ function sendCitySelection(cityName) {
     console.log(cityName)
     // Implement API call to select city by name
     // Example: fetchDataFromAPI({ city: cityName });
-    // console.log("Selected City:", cityName);
 }
 
 function sendPolygonSelection(polygonGeoJSON) {
@@ -191,5 +164,28 @@ function sendPolygonSelection(polygonGeoJSON) {
 
 .search-attribution {
     display: none;
+}
+
+.results-list {
+    background-color: white;
+    border: 1px solid #ccc;
+    padding: 8px;
+    border-radius: 0 0 4px 4px;
+    max-width: 500px;
+}
+
+.result {
+    display: flex;
+    flex-direction: row;
+
+}
+
+.result .result-extra {
+    margin-left: 3px;
+    color: var(--gray-light);
+}
+
+.mapbox-gl-draw_ctrl-draw-btn {
+    color: var(--primary-dark);
 }
 </style>
