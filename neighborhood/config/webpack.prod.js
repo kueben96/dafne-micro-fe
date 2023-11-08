2a73b6428073ab40c0e7c47a706d834a7bb02812
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-08 16:24:57 +0100

    add prod config neighborhood

--- neighborhood/config/webpack.prod.js
+++ neighborhood/config/webpack.prod.js
@@ -1,20 +1,30 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const prodConfig = {
     mode: 'production',
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
     output: {
         filename: '[name].[contenthash].js',
         publicPath: '/neighborhood/latest/'
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
         new ModuleFederationPlugin({
             name: 'neighborhood',
             filename: 'remoteEntry.js',
             exposes: {
                 './NeighborhoodApp': './src/bootstrap'
+            },
+            remotes: {
+                theme: `theme@${domain}/theme/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies
         })
