e3598672eb6a69920e8a3228862e481485ae567d
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-08 16:29:56 +0100

    add domain to prod config

--- neighborhood/config/webpack.prod.js
+++ neighborhood/config/webpack.prod.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-
+const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode: 'production',
     resolve: {
