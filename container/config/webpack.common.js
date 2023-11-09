6fd15f7cd3ed4656918c7f0c4c5b1eabd7b1f1fa
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-09 15:24:36 +0100

    increase sleep time

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
                 use: [
                     {
                         loader: 'file-loader',
-                    },
+                    }150
                 ],
             }
         ]
