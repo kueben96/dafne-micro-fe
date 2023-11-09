2ae3c12c4e098c87ae766a7b81f81684b9e3fe6b
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-09 15:57:41 +0100

    amk

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -4,24 +4,24 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json')
 
 const domain = process.env.PRODUCTION_DOMAIN;
-// production
+
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
         publicPath: '/container/latest/'
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-                dafne: `dafne@${domain}/dafne/latest/remoteEntry.js`
-            },
-            shared: packageJson.dependencies,
-        })
-    ]
+    }, amk!!!!!!
+plugins: [
+    new ModuleFederationPlugin({
+        name: 'container',
+        remotes: {
+            marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+            auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+            dafne: `dafne@${domain}/dafne/latest/remoteEntry.js`
+        },
+        shared: packageJson.dependencies,
+    })
+]
 }
 
 module.exports = merge(commonConfig, prodConfig);
