4fa7e107c54e6f187f04710804bc1c50c62a17a6
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-11-16 15:53:13 +0100

    serve all with serve

--- theme/src/shared-palette.js
+++ theme/src/shared-palette.js
@@ -1,9 +1,15 @@
 const palette = {
     primary: {
+        // blue
         main: '#6CC1B5',
         dark: '#3C9085',
         light: '#A9DBD4',
         lighter: '#E2F3F1'
+        //red
+        // main: '#E35436',
+        // dark: '#8E3522',
+        // light: '#ED9481',
+        // lighter: '#F8D4CD'
     },
     secondary: {
         main: '#E08542',
